a7761ca9f18914848e878f31fb723db8f004d3a1
IPKT <putu.kembar@gmail.com>
2024-01-08 17:39:47 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,4 @@
+<script>
+  // window.location.assign("http://172.20.37.105/geofisikaDNP/profileWeb/beranda");
+  window.location.assign("http://182.23.56.142:90/geofisikaDNP/profileWeb/beranda");
+</script>
